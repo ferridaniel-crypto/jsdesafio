e3017d542d3344cd82d85cadad2edf84065910dd
ferridaniel-crypto <121793237+ferridaniel-crypto@users.noreply.github.com>
2023-05-22 21:12:08 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let numberOne = prompt("digite um numero")
+let numberTwo = prompt("digite outro numero")
+
+let result = Number (numberOne) + Number(numberTwo)
+let result1 = Number (numberOne) - Number(numberTwo)
+let result2 = Number (numberOne) * Number(numberTwo)
+let result3 = Number (numberOne) / Number(numberTwo)
+let result4 = Number (numberOne) % Number(numberTwo)
+
+
+alert (result)
+alert (result1)
+alert (result2)
+alert (result3)
+alert (result4)
+
+if (numberOne === numberTwo) {
+    alert("os numero é igual meu fi")
+} else {
+    alert("os numero nem é igual em parça")
+}
